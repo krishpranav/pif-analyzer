50f967ca67c28b643d8f2d96ac161b90e6b67ae7
krishpranav <krisna.pranav@gmail.com>
2021-05-02 09:43:52 +0530

    update apiKeys.py with strings

---
+++ common/apiKeys.py
@@ -0,0 +1,17 @@
+# Abuse api key
+abuseKey = ''
+
+# hybrid api key
+hybridKey = ''
+
+# mal share api key
+malshareKey = ''
+
+# url scan key 
+urlScanKey = ''
+
+# get valhalla key
+valhallaKey = ''
+
+# virus total api key
+vtKey = ''
